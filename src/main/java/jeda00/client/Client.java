a0258c4def5ca764b4576b772c0b3bbe825e2b5a
Adam Jedlička <adajedlicka@gmail.com>
2019-09-16 16:53:28 +0200

    Close client's scanner

--- src/main/java/jeda00/client/Client.java
+++ src/main/java/jeda00/client/Client.java
@@ -30,6 +30,8 @@ public class Client {
             String line = scanner.nextLine();
             printStream.println(line);
         }
+
+        scanner.close();
     }
 
     public static void main(String[] args) {
